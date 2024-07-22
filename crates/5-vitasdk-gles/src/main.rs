307c9459214846d22b7e5911da00a42ac0672d31
Nikolay Arhipov <n@arhipov.net>
2024-07-22 11:24:20 +0300

    Added sdl+vitagl example

---
+++ crates/5-vitasdk-gles/src/main.rs
@@ -0,0 +1,43 @@
+#[link(name = "SDL2", kind = "static")]
+#[link(name = "vitaGL", kind = "static")]
+#[link(name = "vitashark", kind = "static")]
+#[link(name = "SceShaccCg_stub", kind = "static")]
+#[link(name = "mathneon", kind = "static")]
+#[link(name = "SceShaccCgExt", kind = "static")]
+#[link(name = "taihen_stub", kind = "static")]
+#[link(name = "SceKernelDmacMgr_stub", kind = "static")]
+#[link(name = "SceIme_stub", kind = "static")]
+extern "C" {}
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl()
+        .build()
+        .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+    }
+}
